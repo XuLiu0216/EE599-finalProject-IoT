61449a2b59745a183f592ac49a82ce2964372a16
Xu Liu <liu654@usc.edu>
2019-02-28 01:48:41 -0800

    phase1.0

--- phase1.py
+++ phase1.py
@@ -1,12 +1,15 @@
 import math
 from datetime import datetime
+
+#The class to represent location
 class loc:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+#user`s class definition
 class UserEntity:
-    def __init__(self, id, currLoc, destination, preference, budget, carModel, date, startTime, endTime):
+    def __init__(self, id, currLoc, destination, preference, budget, date, startTime, endTime):
         self.id = id
         self.currLoc = currLoc
         #pref = 0 means prefer mincost, pref = 1 means prefer min distance
@@ -14,15 +17,16 @@ class UserEntity:
         #user`s budget for parking
         self.budget = budget
         self.destLoc = destination
-        self.carModel = carModel
         self.date = date
         self.startTime = startTime
         self.endTime = endTime
         #the parking choice list for this user
-        self.plist = []
+        self.pList = []
+        self.opt = -1;
 
+#parking entity`s class
 class ParkingEntities:
-    def __init__(self, id, loc, charge, maxTime, workday, startTime, endTime, numCompact, numRegular):
+    def __init__(self, id, loc, charge, maxTime, workday, startTime, endTime, numSpot):
         self.id = id
         self.loc = loc
         self.charge = charge
@@ -30,19 +34,20 @@ class ParkingEntities:
         self.workday = workday
         self.startTime = startTime
         self.endTime = endTime
-        self.numCompact = numCompact
-        self.numRegular = numRegular
-        #the user list who would choose this parking structure as an optimal choice
+        self.numSpot = numSpot;
+        # the user list who would choose this parking structure as an optimal choice
+        self.count = 0
         self.uList = []
 
+#The class to represent the interaction between user and parking
 class interaction:
 
     def __init__(self, user, parking):
         self.parking = parking
         self.user = user
-
+    #calculate the distance between user`s current location and the parking structure
     def distance_user_parking(self):
-        dis = math.sqrt((self.parking.loc.x - self.user.loc.x) ** 2 + (self.parking.loc.y - self.user.loc.y) ** 2)
+        dis = math.sqrt((self.parking.loc.x - self.user.currLoc.x) ** 2 + (self.parking.loc.y - self.user.currLoc.y) ** 2)
         return dis
 
     #distance between paking structure and destination
@@ -50,6 +55,7 @@ class interaction:
         dis = math.sqrt((self.parking.loc.x - self.user.destLoc.x) ** 2 + (self.parking.loc.y - self.user.destLoc.y) ** 2)
         return dis
 
+    #the function to calculate the cost if the user park in this parking entity
     def cost(self):
         #0215 means 2$/hour max 15$/day, assume total cost less than 100$
         hourCharge = int(self.parking.charge[0])*10 + int(self.parking.charge[1])
@@ -64,49 +70,25 @@ class interaction:
             else:
                 return hourCharge * (self.user.endTime - self.user.startTime)
     #check if there is avaliable parking spots in self parking
-    def check_avaliable(self):
-        #model only includes'S' and 'R', S means small, R means regular.
-        #compact paking spaces only for small cars, and regular paking spots serve for small and regular cars both
-        model = self.user.carModel
-        if(model =='S' and (self.parking.numCompact + self.parking.numRegular) <= 0):
-            return False
-        elif(model == 'R' and self.parking.numRegular <= 0):
+    def check(self):
+        if self.parking.numSpot <= 0:
             return False
         else:
             weekday = datetime.strptime(self.user.date, "%Y%m%d").weekday()
             if(self.parking.workday[weekday] == '1'):
                 if(self.user.startTime >= self.parking.startTime and self.user.endTime <= self.parking.endTime and self.user.endTime - self.user.startTime < self.parking.maxTime):
-                    return True
+                    if self.cost() <= self.user.budget and self.walk_distance() <= 1000:
+                        return True
             return False
 
-    #final check : if the avaliable parking spot suit for user`s preference
-    def check(self):
-        if(self.check_avaliable()):
-            if(self.user.pref == 0):
-                print self.cost()
-                if(self.cost() <= self.user.budget):
-                    return True
-            else:
-                #assume we only choose the parking less than 1000 meters
-                if(self.walk_distance() <= 10000):
-                    return True
-        return False
-
-
-
-    def __lt__(self, other):
-        if self.walk_distance() < other.walk_distance():
-            return True;
-        else:
-            return False;
-
+#read the input and save the data
 def read_input():
     filename = 'userInput.txt'
     with open(filename, 'r') as f:
         line = f.readline()
-        users = []
+        users = {}
         while line:
-            id = line[0:4]
+            id = int(line[0:4])
             userX = int(line[4:8])
             userY = int(line[8:12])
             userLoc = loc(userX, userY)
@@ -114,21 +96,20 @@ def read_input():
             destY = int(line[16:20])
             destLoc = loc(destX, destY)
             pref = int(line[20:21])
-            budget = int(line[21:23])
-            model = line[23:24]
-            date = line[24:32]
-            start = int(line[32:34])
-            end = int(line[34:36])
-            user = UserEntity(id, userLoc, destLoc, pref, budget, model, date, start, end)
-            users.append(user)
+            budget = int(line[20:22])
+            date = line[22:30]
+            start = int(line[30:32])
+            end = int(line[32:34])
+            user = UserEntity(id, userLoc, destLoc, pref, budget, date, start, end)
+            users[id] = user
             line = f.readline()
     f.close()
     filename2 = 'parkingInput.txt'
     with open(filename2, 'r') as f:
         line = f.readline()
-        parkings = []
+        parkings = {}
         while line:
-            id = line[0:4]
+            id = int(line[0:4])
             parkingX = int(line[4:8])
             parkingY = int(line[8:12])
             parkingLoc = loc(parkingX, parkingY)
@@ -137,27 +118,86 @@ def read_input():
             workday = line[18:25]
             start = int(line[25:27])
             end = int(line[27:29])
-            numCompact = int(line[29:33])
-            numRegular = int(line[33:37])
+            numSpot = int(line[29:33])
             line = f.readline()
-            parking = ParkingEntities(id, parkingLoc, charge, maxtime, workday, start, end, numCompact, numRegular)
-            parkings.append(parking)
+            parking = ParkingEntities(id, parkingLoc, charge, maxtime, workday, start, end, numSpot)
+            parkings[id] = parking
     f.close()
     return users, parkings
 
+#the process to allocate parking to users
+def allocate(U, P):
+    u1 = U[1]
+    tempList = []
+    M = [[0 for col in range(len(P))] for row in range(len(U))]
+    Res = [[0 for col in range(len(P))] for row in range(len(U))]
+    pRank = []
+    for i in range(len(P)):
+        temp = interaction(u1, P[i + 1])
+        tempList.append(temp)
+    tempList.sort(key=lambda interaction: interaction.walk_distance())
+    for t in tempList:
+        print "sss", t.parking.id,t.walk_distance()
+        pRank.append(t.parking.id)
+
+    for m in range(len(U)):
+        u = U[m + 1]
+        for n in range(len(P)):
+            p = P[n + 1]
+            i = interaction(u, p)
+            if(i.check()):
+                M[u.id - 1][p.id - 1] = 1
+                p.uList.append(u)
+                p.count += 1
+                Res[u.id - 1][p.id - 1] = 0
+            else:
+                Res[u.id - 1][p.id - 1] = 0
+
+    recursiveFunction(U, P, pRank, M , Res)
+    print Res
+    return
+
+#The recursive function to solve this problem recursively from the most optimal parking to the least optimal one
+#Every time we enter this function we only check the first parking structure in pRank, if there is enough spots,
+#the allocate, if not ,then sort all distance between the user who is valid to this parking entity and this
+#specific parking
+def recursiveFunction(U, P, pRank, M, R):
+    #base case to stop
+    if len(U) == 0 or len(P) == 0:
+        return
+    id = pRank[0]
+    #no conflict:
+    print pRank, P
+    print P[id].count, "count"
+    #if there is enough parking spots here
+    if P[id].count <= P[id].numSpot:
+        for key in U.keys():
+            if M[key - 1][id - 1] == 1:
+                R[key - 1][id - 1] = 1
+                U[key].opt = id
+                U.pop(key)
+    #if not ,which means there will be several users who wants the same spot => conflict:
+    else:
+        IA = []
+        for key in U.keys():
+            if M[key - 1][id - 1] == 1:
+                ia = interaction(U[key], P[id])
+                IA.append(ia)
+        #sort all the users by the distance between user and parking
+        IA.sort(key=lambda interaction: interaction.distance_user_parking())
+        for j in range(P[id].numSpot):
+            userID = IA[j].user.id
+            R[userID - 1][id - 1] = 1
+            U[userID].opt = id
+            U.pop(userID)
+            for k in range(id, len(M[0])):
+                M[userID - 1][k] = 0
+    #delete the parking entity we checked in this round and step into next round
+    P.pop(id)
+    del(pRank[0])
+    return recursiveFunction(U, P, pRank, M, R)
+
 
 U,P = read_input()
-#UI is the list of list of interactions for each user
-UI = []
-for u in U:
-    #I is the list of interactions for this user u
-    I = []
-    for p in P:
-        i = interaction(u, p)
-        if(i.check()):
-            I.append(i)
-    if(I[0].user.pref == 0):
-        I.sort(key = lambda interaction: interaction.cost())
-    else:
-        I.sort(key = lambda interaction: interaction.walk_distance())
-    UI.append(I)
+allocate(U,P)
+
